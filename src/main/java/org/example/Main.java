3be0478d933adc3f2ca023e37ba7ae9793a27021
Peeyush245 <peeyushpandey245@gmail.com>
2023-02-13 21:18:33 +0530

    File Handling in Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        createNewFile();
+        writeToFile();
+        readFile();
+    }
+
+    private static void createNewFile() {
+        try {
+            File file = new File("Log/log.txt");
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeToFile() {
+        try {
+            FileWriter writer = new FileWriter("Log/log.txt");
+            writer.write("Insert produced log in the file");
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void readFile() {
+        try {
+            File file = new File("Log/log.txt");
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
